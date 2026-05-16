2c5392375a520419071ce2c0b3a00fa62ceac1bf
Alexandr-VA <alexandrlobash@gmail.com>
2026-05-16 16:13:30 +0300

    Add files via upload

---
+++ Import_Export/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'] 
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  mode: 'development',
+  devServer: {
+  static: './dist',
+  port: 3000,
+  open: true,
+  hot: true,
+  proxy: [
+    {
+      context: ['/api'],
+      target: 'http://localhost:5000',
+    },
+  ],
+},
+};
